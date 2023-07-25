45f8adba64fef0a5ea4b3c0c4ec7cd9ab52998f4
mohammed adel <hamadygg66@gmail.com>
2023-07-25 10:07:24 +0200

    Initial Commit

---
+++ intro-card/intro_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  intro_cardApp.swift
+//  intro-card
+//
+//  Created by mohmmed adel on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct intro_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
